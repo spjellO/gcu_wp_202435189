f2c416c0b1c2922200650743bfbfea32642442e3
채주원 <wndnjs4698@naver.com>
2024-03-22 16:37:07 +0900

    laptop

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
@@ -9,3 +10,7 @@ def root():
 @app.get("/test")
 def test():
     return "Here is testing page"
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
