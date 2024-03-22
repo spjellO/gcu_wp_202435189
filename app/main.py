9a50c9d384431baa82f4bca8c5b8bd99227b580e
채주원 <wndnjs4698@naver.com>
2024-03-22 16:06:23 +0900

    change main

--- app/main.py
+++ app/main.py
@@ -6,10 +6,6 @@ app = FastAPI()
 def root():
     return { "message": "Hello Root!" }
 
-@app.get("/home")
-def home():
-    return { "message": "Home!" }
-
 @app.get("/test")
 def test():
     return "Here is testing page"
